7521168b04f7072ae48fb673e8017933f25a2337
nagachandu2003 <nagachanduthatha@gmail.com>
2024-06-14 19:32:18 +0530

    added s3 storage

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ObjectId } = require("mongodb");
 const dotenv = require("dotenv");
+const multer = require("multer");
+const AWS = require("aws-sdk");
+const {v4:uuidv4} = require("uuid");
 
 
 
@@ -9,6 +12,46 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 dotenv.config();
+
+// AWS S3 File Upload Code Snippet Starts
+const s3 = new AWS.S3({
+  accessKeyId:process.env.AWS_ID,
+  secretAccessKey:process.env.AWS_SECRET
+})
+
+const storage = multer.memoryStorage({
+    destination: function(req,file,callback){
+      callback(null,'')
+    }
+})
+
+const upload = multer({storage}).single('file')
+
+app.post("/upload",upload,(req,res) => {
+  let myFile = req.file.originalname.split(".")
+  const fileType = myFile[myFile.length-1]
+
+  const params = {
+    Bucket : process.env.AWS_BUCKET_NAME,
+    Key : `${uuidv4()}.${fileType}`,
+    Body : req.file.buffer
+  }
+  
+  s3.upload(params, (error,data) => {
+    if(error){
+      res.status(500).send({Error : error})
+    }
+    res.status(200).send(data)
+  })
+})
+
+// AWS S3 File Upload Code Snippet Ends
+
+
+
+
+
+
 
 const uri = process.env.mongo_uri;
 const client = new MongoClient(uri);
